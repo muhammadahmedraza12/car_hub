b8070e30878831043d82a0bf0db837ca426a661c
muhammadahmedraza12 <ahmeedr916789@gmail.com>
2023-10-12 05:55:27 +0500

    initial commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link"
+import Image from "next/image"
+import  CustomButton  from "./CustomButton"
+
+const Navbar = () => {
+  return (
+   <header className="w-full absolute z-10">
+    <nav className="max-w-[1440px mx-auto flex justify-between items-center sm:px-16 px-6 py-4">
+        <Link href="/" className="flex justify-center items-center">
+            <Image src="/logo6.svg" alt="Car Hub Logo"  width={138} 
+            height={28} className="object_contain " />
+        </Link>
+
+        <CustomButton title="Sign In" btnType="button" containerStyles="text-white rounded-full bg-white min-w-[130px]" />
+    </nav>
+   </header>
+  )
+}
+
+export default Navbar
